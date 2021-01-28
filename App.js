fbdd47740a6f5fe2523be720a5035707e5a73c75
zean.ma <zean.ma@oyohotels.cn>
2021-01-28 11:52:10 +0800

    feat:自定义button组件

--- App.js
+++ App.js
@@ -13,21 +13,10 @@ import {
   Text,
   SafeAreaView,
   StyleSheet,
-  TouchableOpacity,
   Platform
 } from 'react-native';
 
-// import {
-//   Header,
-//   LearnMoreLinks,
-//   Colors,
-//   DebugInstructions,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
-
-// import Hello from './app/test/Hello';
-import Greet from './app/test/Greet';
-
+import FlexButton from './app/test/FlexButton';
 
 export default App = () => {
   const [count, setCount] = useState(0);
@@ -43,9 +32,7 @@ export default App = () => {
         <Text>you checked {count} times</Text>
       </View>
       <View>
-        <TouchableOpacity style={styles.button} onPress={onPress}>
-          <Text style={styles.btnText}>Press Here</Text>
-        </TouchableOpacity>
+        <FlexButton onPress={onPress}/>
       </View>
     </SafeAreaView>
   );
@@ -59,60 +46,8 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
     paddingHorizontal: 10,
   },
-  button: {
-    alignItems: "center",
-    backgroundColor: "#303846",
-    padding: 10,
-    margin: Platform.OS == 'android' ? 2 : 10,
-    marginBottom: Platform.OS == 'ios' ? 10 : 5,
-    borderRadius: 10
-  },
   countContainer: {
     alignItems: 'center',
     padding: 10
-  },
-  btnText: {
-    color: 'white',
-    fontSize: 15,
-
   }
 })
-
-// const styles = StyleSheet.create({
-//   scrollView: {
-//     backgroundColor: Colors.lighter,
-//   },
-//   engine: {
-//     position: 'absolute',
-//     right: 0,
-//   },
-//   body: {
-//     backgroundColor: Colors.white,
-//   },
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//     color: Colors.black,
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//     color: Colors.dark,
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-//   footer: {
-//     color: Colors.dark,
-//     fontSize: 12,
-//     fontWeight: '600',
-//     padding: 4,
-//     paddingRight: 12,
-//     textAlign: 'right',
-//   },
-// });
---
+++ app/test/FlexButton.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    Platform
+} from 'react-native';
+
+
+const FlexButton = (props) => {
+    return (
+        <TouchableOpacity style={styles.button} onPress={props.onPress}>
+            <Text style={styles.btnText}>Press Here</Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    button: {
+        alignItems: "center",
+        backgroundColor: "#303846",
+        padding: 10,
+        margin: Platform.OS == 'android' ? 2 : 10,
+        marginBottom: Platform.OS == 'ios' ? 10 : 5,
+        borderRadius: 10
+    },
+    btnText: {
+        color: 'white',
+        fontSize: 15,
+    }
+})
+
+export default FlexButton;
