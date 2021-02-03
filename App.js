585552bcabdcda7e0510bfd280da532fd1ce7106
zean.ma <zean.ma@oyohotels.cn>
2021-02-03 18:48:22 +0800

    控制背景图层的训练

--- App.js
+++ App.js
@@ -6,39 +6,51 @@
  * @flow strict-local
  */
 
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 
 import {
   View,
   Text,
   SafeAreaView,
   StyleSheet,
-  Platform
+  Platform,
+  ActivityIndicator,
 } from 'react-native';
 
 import FlexButton from './app/components/FlexButton';
 
 export default App = () => {
   const [count, setCount] = useState(0);
+  const [login, setLogin] = useState("未登陆");
 
   onPress = () => {
     setCount(count + 1)
+    setLogin("登陆成功!")
   }
   onRefesh = () => {
     setCount(0)
+    setLogin("注册成功!")
   }
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.countContainer}>
-        <Text style={styles.text}>you checked {count} times</Text>
+      <View style={styles.textContainer}>
+        {/* <Text style={styles.text}>you checked {count} times</Text> */}
+        <Text style={styles.text}>{login}</Text>
+        <Text style={styles.text}>成功 +{count}次</Text>
       </View>
-      <View>
-        <FlexButton onPress={onPress} title='Press me'/>
+      <View style={styles.aiContainer}>
+        <ActivityIndicator size="small" color="#0000ff" />
       </View>
-      <View>
-        <FlexButton onPress={onRefesh} title='Refresh'/>
+      <View style={styles.btnContainer}>
+        <View>
+          <FlexButton onPress={onPress} title='登陆' />
+        </View>
+        <View>
+          <FlexButton onPress={onRefesh} title='注册' />
+        </View>
       </View>
+     
     </SafeAreaView>
   );
 }
@@ -46,12 +58,25 @@ export default App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-end',
-    paddingHorizontal: 10,
+    justifyContent: 'flex-end'
   },
-  countContainer: {
+  textContainer: {
+    flex: 2,
+    backgroundColor: '#6784AF',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
-    padding: 10
+  },
+  btnContainer: {
+    flex: 3,
+    backgroundColor: '#A2DAE0',
+    justifyContent: 'flex-end',
+    alignItems: 'stretch',
+  },
+  aiContainer: {
+    flex: 1,
+    backgroundColor: '#A2DAE0',
+    justifyContent: 'flex-end',
+    alignItems: 'stretch',
   },
   text: {
     fontSize: 24,
